9d0bce4bfe53ccf38521e49897e482ffc28ff2c4
Saitoria <saronisaitoria@gmail.com>
2022-01-27 20:24:21 +0300

    App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppNavigator from './app/navigation/AppNavigator';
+import ListItem from './app/components/ListItem';
+
+
+export default function App()
+{
+    return(
+        <AppNavigator/>
+        //<ListItem/>
+    );
+}
+
